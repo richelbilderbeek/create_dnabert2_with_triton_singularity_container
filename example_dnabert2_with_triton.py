73e1fd0b992b9392f9de09f98571d1c91bb96f85
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-09-06 16:27:59 +0200

    Fix script

--- example_dnabert2_with_triton.py
+++ example_dnabert2_with_triton.py
@@ -1,5 +1,11 @@
 print('Hello world')
 
+import torch
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+model = AutoModel.from_pretrained("zhihan1996/DNABERT-2-117M", trust_remote_code=True)
+
 from transformers.models.bert.configuration_bert import BertConfig
 
 config = BertConfig.from_pretrained("zhihan1996/DNABERT-2-117M")
